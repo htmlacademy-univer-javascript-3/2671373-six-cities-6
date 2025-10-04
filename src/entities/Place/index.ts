4dbc4138a73ff4c96fab22e89ad84bcb1795bca4
Skoptenco <bochkovruslan01@gmail.com>
2025-10-04 23:19:18 +0300

    feat: - added Place entity - added PlaceCard ui

---
+++ src/entities/Place/index.ts
@@ -0,0 +1,3 @@
+export * from './mocks';
+export * from './model';
+export * from './ui';
---
+++ src/entities/Place/ui/PlaceCard/PlaceCard.tsx
@@ -0,0 +1,45 @@
+import {FC} from 'react';
+import {IPlaceCard} from './PlaceCard.type.ts';
+
+const PlaceCard: FC<IPlaceCard> = (props) => {
+
+  const { place } = props;
+
+  return (
+    <article className="cities__card place-card">
+      <div className="cities__image-wrapper place-card__image-wrapper">
+        <a href="#">
+          <img className="place-card__image" src={place.img} width="260" height="200" alt="Place image"/>
+        </a>
+      </div>
+      <div className="place-card__info">
+        <div className="place-card__price-wrapper">
+          <div className="place-card__price">
+            <b className="place-card__price-value">&euro;{place.price}</b>
+            <span className="place-card__price-text">&#47;&nbsp;night</span>
+          </div>
+          <button className="place-card__bookmark-button place-card__bookmark-button--active button"
+            type="button"
+          >
+            <svg className="place-card__bookmark-icon" width="18" height="19">
+              <use xlinkHref="#icon-bookmark"></use>
+            </svg>
+            <span className="visually-hidden">In bookmarks</span>
+          </button>
+        </div>
+        <div className="place-card__rating rating">
+          <div className="place-card__stars rating__stars">
+            <span style={{width: '80%'}}></span>
+            <span className="visually-hidden">Rating</span>
+          </div>
+        </div>
+        <h2 className="place-card__name">
+          <a href="#">{place.title}</a>
+        </h2>
+        <p className="place-card__type">{place.type}</p>
+      </div>
+    </article>
+  );
+};
+
+export default PlaceCard;
