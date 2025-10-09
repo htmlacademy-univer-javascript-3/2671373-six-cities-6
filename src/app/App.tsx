5a23a7f10e69dcc949d3dd92b0bfb5460fd63720
Skoptenco <bochkovruslan01@gmail.com>
2025-10-09 17:43:38 +0300

    feat: - added other pages

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,20 @@
 import {FC} from 'react';
 import { MainPage } from '@/pages/Main';
+// import {OfferNotLoggedPage} from '@/pages/OfferNotLogged';
+// import {LoginPage} from '@/pages/Login';
+// import {FavoritesPage} from '@/pages/Favorites';
+// import {FavoritesEmpty} from '@/pages/FavoritesEmpty';
+// import {OfferPage} from '@/pages/Offer';
+// import {MainEmptyPage} from '@/pages/MainEmpty';
 
 const App: FC<{placesCount: number}> = ({placesCount}) => (
+  // <LoginPage/>
+  // <FavoritesPage/>
+  // <FavoritesEmpty/>
+  // <OfferPage />
+  // <OfferNotLoggedPage/>
   <MainPage placesCount={placesCount}/>
+  // <MainEmptyPage/>
 );
 
 export default App;
--- src/pages/MainEmpty/ui/MainEmpty.tsx
+++ src/pages/MainEmpty/ui/MainEmpty.tsx
@@ -1,7 +1,6 @@
-import {PlaceCard, placesMock} from '@/entities/Place';
 import {FC} from 'react';
 
-const MainPage: FC<{placesCount: number}> = ({placesCount}) => (
+const MainEmptyPage: FC = () => (
   <div className="page page--gray page--main">
     <header className="header">
       <div className="container">
@@ -32,7 +31,7 @@ const MainPage: FC<{placesCount: number}> = ({placesCount}) => (
       </div>
     </header>
 
-    <main className="page__main page__main--index">
+    <main className="page__main page__main--index page__main--index-empty">
       <h1 className="visually-hidden">Cities</h1>
       <div className="tabs">
         <section className="locations container">
@@ -53,7 +52,7 @@ const MainPage: FC<{placesCount: number}> = ({placesCount}) => (
               </a>
             </li>
             <li className="locations__item">
-              <a className="locations__item-link tabs__item tabs__item--active">
+              <a className="locations__item-link tabs__item">
                 <span>Amsterdam</span>
               </a>
             </li>
@@ -63,7 +62,7 @@ const MainPage: FC<{placesCount: number}> = ({placesCount}) => (
               </a>
             </li>
             <li className="locations__item">
-              <a className="locations__item-link tabs__item" href="#">
+              <a className="locations__item-link tabs__item tabs__item--active" href="#">
                 <span>Dusseldorf</span>
               </a>
             </li>
@@ -71,38 +70,18 @@ const MainPage: FC<{placesCount: number}> = ({placesCount}) => (
         </section>
       </div>
       <div className="cities">
-        <div className="cities__places-container container">
-          <section className="cities__places places">
-            <h2 className="visually-hidden">Places</h2>
-            <b className="places__found">{placesCount} places to stay in Amsterdam</b>
-            <form className="places__sorting" action="#" method="get">
-              <span className="places__sorting-caption">Sort by</span>
-              <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                <svg className="places__sorting-arrow" width="7" height="4">
-                  <use xlinkHref="#icon-arrow-select"></use>
-                </svg>
-              </span>
-              <ul className="places__options places__options--custom places__options--opened">
-                <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-                <li className="places__option" tabIndex={0}>Price: low to high</li>
-                <li className="places__option" tabIndex={0}>Price: high to low</li>
-                <li className="places__option" tabIndex={0}>Top rated first</li>
-              </ul>
-            </form>
-            <div className="cities__places-list places__list tabs__content">
-              {placesMock.map((place) => (
-                <PlaceCard place={place} key={place.id} />
-              ))}
+        <div className="cities__places-container cities__places-container--empty container">
+          <section className="cities__no-places">
+            <div className="cities__status-wrapper tabs__content">
+              <b className="cities__status">No places to stay available</b>
+              <p className="cities__status-description">We could not find any property available at the moment in Dusseldorf</p>
             </div>
           </section>
-          <div className="cities__right-section">
-            <section className="cities__map map"></section>
-          </div>
+          <div className="cities__right-section"></div>
         </div>
       </div>
     </main>
   </div>
 );
 
-export default MainPage;
+export default MainEmptyPage;
