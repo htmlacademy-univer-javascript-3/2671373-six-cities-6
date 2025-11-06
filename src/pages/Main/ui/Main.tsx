1a7a71f8c1e69b0dc647bb4d63d1ba3744471bf7
Skoptenco <bochkovruslan01@gmail.com>
2025-11-06 14:51:52 +0300

    feat: - locations list component

--- src/pages/Main/ui/Main.tsx
+++ src/pages/Main/ui/Main.tsx
@@ -1,54 +1,35 @@
 import {OffersList} from '@/entities/Offer';
 import {currentCityMock, offersMock, pointsMock, selectedPointMock} from '@/shared/mocks';
-import {FC, useState} from 'react';
+import {FC, useLayoutEffect, useState} from 'react';
 import {Map} from '@/widgets/Map/ui';
+import {LocationsList} from './components/LocationsList';
+import {useSearchParams} from 'react-router-dom';
+
+const locations = [
+  'Paris', 'Cologne', 'Brussels', 'Amsterdam', 'Hamburg', 'Dusseldorf'
+];
 
 const MainPage: FC = () => {
+
+  const [searchParams] = useSearchParams();
 
   const [currentCity] = useState(currentCityMock);
   const [points] = useState(pointsMock);
   const [selectedPoint] = useState(selectedPointMock);
+  const [activeLocation, setActiveLocation] = useState(searchParams.get('location') || locations[0]);
+
+  useLayoutEffect(() => {
+    const location = searchParams.get('location');
+    if (location) {
+      setActiveLocation(location);
+    }
+  }, [searchParams]);
 
   return (
     <div className="page page--gray page--main">
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
-        <div className="tabs">
-          <section className="locations container">
-            <ul className="locations__list tabs__list">
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Paris</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Cologne</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Brussels</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item tabs__item--active">
-                  <span>Amsterdam</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Hamburg</span>
-                </a>
-              </li>
-              <li className="locations__item">
-                <a className="locations__item-link tabs__item" href="#">
-                  <span>Dusseldorf</span>
-                </a>
-              </li>
-            </ul>
-          </section>
-        </div>
+        <LocationsList locations={locations} active={activeLocation}/>
         <div className="cities">
           <div className="cities__places-container container">
             <section className="cities__places places">
---
+++ src/pages/Main/ui/components/LocationsList/LocationsList.tsx
@@ -0,0 +1,31 @@
+import {FC} from 'react';
+import * as classNames from 'classnames';
+import {Link} from 'react-router-dom';
+
+interface ILocationsList {
+  locations: string[];
+  active: string;
+}
+
+const LocationsList: FC<ILocationsList> = (props) => {
+
+  const { active, locations } = props;
+
+  return (
+    <div className="tabs">
+      <section className="locations container">
+        <ul className="locations__list tabs__list">
+          {locations.map((location) => (
+            <li className="locations__item" key={location}>
+              <Link className={classNames('locations__item-link tabs__item', active === location && 'tabs__item--active')} to={`?location=${location}`}>
+                <span>{location}</span>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </div>
+  );
+};
+
+export default LocationsList;
