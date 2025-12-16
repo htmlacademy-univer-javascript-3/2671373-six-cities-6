16af3431987f1edd38051b9c8661097d857705fa
Skoptenco <bochkovruslan01@gmail.com>
2025-12-16 14:06:58 +0300

    refactoring: - component CommentOffer

--- src/pages/Offer/ui/components/Reviews/Reviews.tsx
+++ src/pages/Offer/ui/components/Reviews/Reviews.tsx
@@ -2,7 +2,7 @@ import {TComment} from '@/shared/model/comment';
 import {FC, memo} from 'react';
 import {LoadingWrapper} from '@/shared/ui/LoadingWrapper';
 import {AddReviewForm} from '@/features/add-review/ui/AddReviewForm';
-import {formatDate} from 'date-fns';
+import {CommentOffer} from '@/entities/Comment/ui';
 
 interface IReviews {
   comments: TComment[];
@@ -21,36 +21,7 @@ const Reviews: FC<IReviews> = memo((props: IReviews) => {
       <LoadingWrapper isLoading={isLoading}>
         <ul className="reviews__list">
           {comments.map((comment) => (
-            <li className="reviews__item" key={comment.id}>
-              <div className="reviews__user user">
-                <div className="reviews__avatar-wrapper user__avatar-wrapper">
-                  <img
-                    className="reviews__avatar user__avatar"
-                    src={comment.user.avatarUrl}
-                    width="54"
-                    height="54"
-                    alt="Reviews avatar"
-                  />
-                </div>
-                <span className="reviews__user-name">{comment.user.name}</span>
-              </div>
-              <div className="reviews__info">
-                <div className="reviews__rating rating">
-                  <div className="reviews__stars rating__stars">
-                    <span style={{width: `${comment.rating * 20}%`}}></span>
-                    <span className="visually-hidden">Rating</span>
-                  </div>
-                </div>
-                <p className="reviews__text">{comment.comment}</p>
-                {/*TODO add date lib*/}
-                <time
-                  className="reviews__time"
-                  dateTime={formatDate(comment.date, 'yyyy-MM-dd')}
-                >
-                  {formatDate(comment.date, 'MMMM yyyy')}
-                </time>
-              </div>
-            </li>
+            <CommentOffer comment={comment} key={comment.id}/>
           ))}
         </ul>
       </LoadingWrapper>
